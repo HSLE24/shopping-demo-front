2bf15942413cb6c20932ad7f4edd5c66a4710306
unknown <sh911124@gmail.com>
2024-05-30 23:37:06 +0900

    resucer modify

--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,8 +1,21 @@
 import * as types from "../constants/user.constants";
-const initialState = {};
+const initialState = {
+  error: "",
+};
 
 function userReducer(state = initialState, action) {
   const { type, payload } = action;
+
+  if (type === types.REGISTER_USER_FAIL) {
+    return { ...state, error: payload };
+  }
+  if (type === types.REGISTER_USER_REQUEST) {
+    return { ...state, error: "" };
+  }
+  if (type === types.REGISTER_USER_SUCCESS) {
+    return { ...state, error: "" };
+  }
+
   return state;
 }
 
